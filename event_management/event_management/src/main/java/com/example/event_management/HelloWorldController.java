bdc3377ddff9d149032356d06fe788ab1d93765f
Gajendra Jangid <89866416+gajendraJangid421@users.noreply.github.com>
2023-05-31 15:35:10 +0530

    Add files via upload

---
+++ event_management/event_management/src/main/java/com/example/event_management/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.event_management;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}

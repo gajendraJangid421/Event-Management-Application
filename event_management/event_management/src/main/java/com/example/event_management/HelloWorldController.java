68bb92b25222465ef30356d2805c879935e8694d
Gajendra Jangid <89866416+gajendraJangid421@users.noreply.github.com>
2023-06-01 20:10:52 +0530

    Update HelloWorldController.java

--- event_management/event_management/src/main/java/com/example/event_management/HelloWorldController.java
+++ event_management/event_management/src/main/java/com/example/event_management/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path ="/api")
 public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {

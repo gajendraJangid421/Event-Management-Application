43490a40ecd90dfcf00ac58d22b7b05090743d3a
Gajendra Jangid <gajendra.jangid4210@gmail.com>
2023-06-05 17:43:43 +0530

    login commit

--- event_management/src/main/java/com/example/event_management/EventManagementApplication.java
+++ event_management/src/main/java/com/example/event_management/EventManagementApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventManagementApplication.class, args);
-
 	}
 
 }

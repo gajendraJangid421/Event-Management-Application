f1e2487d3d9ba1e17ac9075fb232b72049ed290a
Gajendra Jangid <gajendra.jangid4210@gmail.com>
2023-06-08 16:37:09 +0530

    Without token

--- event_management/src/main/java/com/example/event_management/model/LoginResponse.java
+++ event_management/src/main/java/com/example/event_management/model/LoginResponse.java
@@ -19,42 +19,25 @@ public class LoginResponse {
     @Column(name = "token_expiry")
     String tokenExpiry;
 
-
     private String message;
     private boolean status;
 
-//    public Session(String loginId, String usersId, String token, String tokenExpiry) {
-//        super();
-//        this.loginId = loginId;
-//        this.usersId = usersId;
-//        this.token = token;
-//        this.tokenExpiry = tokenExpiry;
-//    }
-
     LoginResponse(){}
 
-    public LoginResponse(String message) {
+    public LoginResponse(String message,String loginId, String usersId, String token, String tokenExpiry) {
         super();
         this.message = message;
 //        this.status = status;
-
         this.loginId = loginId;
         this.usersId = usersId;
         this.token = token;
         this.tokenExpiry = tokenExpiry;
     }
 
-//    public LoginResponse(String message,String loginId, String usersId, String token, String tokenExpiry) {
+//    public LoginResponse(String message) {
 //        super();
 //        this.message = message;
-////        this.status = status;
-//
-//        this.loginId = loginId;
-//        this.usersId = usersId;
-//        this.token = token;
-//        this.tokenExpiry = tokenExpiry;
 //    }
-
 
     public String getLoginId() {
         return loginId;
@@ -115,69 +98,4 @@ public class LoginResponse {
                 ", tokenExpiry='" + tokenExpiry + '\'' +
                 '}';
     }
-
-//    @Entity
-//    @Table(name = "session")
-//    public static class Session {
-//
-//        @Id
-//        @Column(name = "login_Id")
-//        String loginId;
-//        @Column(name = "users_Id")
-//        String usersId;
-//        @Column(name = "token")
-//        String token;
-//        @Column(name = "token_expiry")
-//        String tokenExpiry;
-//
-//        public Session(String loginId, String usersId, String token, String tokenExpiry) {
-//            super();
-//            this.loginId = loginId;
-//            this.usersId = usersId;
-//            this.token = token;
-//            this.tokenExpiry = tokenExpiry;
-//        }
-//
-//        public String getLoginId() {
-//            return loginId;
-//        }
-//
-//        public void setLoginId(String loginId) {
-//            this.loginId = loginId;
-//        }
-//
-//        public String getUsersId() {
-//            return usersId;
-//        }
-//
-//        public void setUsersId(String usersId) {
-//            this.usersId = usersId;
-//        }
-//
-//        public String getToken() {
-//            return token;
-//        }
-//
-//        public void setToken(String token) {
-//            this.token = token;
-//        }
-//
-//        public String getTokenExpiry() {
-//            return tokenExpiry;
-//        }
-//
-//        public void setTokenExpiry(String tokenExpiry) {
-//            this.tokenExpiry = tokenExpiry;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "Session{" +
-//                    "loginId='" + loginId + '\'' +
-//                    ", usersId='" + usersId + '\'' +
-//                    ", token='" + token + '\'' +
-//                    ", tokenExpiry='" + tokenExpiry + '\'' +
-//                    '}';
-//        }
-//    }
 }
--- event_management/src/main/java/com/example/event_management/service/LoginService.java
+++ event_management/src/main/java/com/example/event_management/service/LoginService.java
@@ -6,7 +6,6 @@ import com.example.event_management.model.Login;
 import com.example.event_management.model.LoginResponse;
 import com.example.event_management.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -19,11 +18,9 @@ public class LoginService {
 
     @Autowired
     private UsersRepository usersRepository;
+
     @Autowired
     private SessionRepository sessionRepository;
-//    private Session session;
-//    private List<Session> sessionList = new ArrayList<>();
-//    boolean flag = false;
 
     //Not able to use same method from UsersService because it needs to be in static
     public Users findByUsername(String username) {
@@ -39,7 +36,7 @@ public class LoginService {
 
     public LoginResponse authenticationOfUser(Login login, LoginResponse session){
 //        Users users = UsersService.findByUsername(login.getUsername());
-        BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
+//        BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
 
         Users users = findByUsername(login.getUsername());
 
@@ -50,22 +47,23 @@ public class LoginService {
 
             if(isPasswordCorrect){
 
-//                session.setLoginId(UUID.randomUUID().toString());
-//                session.setUsersId(users.getUsersId());
-//                session.setTokenExpiry(Timestamp.from(Instant.now()).toString());
-//                session = sessionRepository.save(session);
+                session.setLoginId(UUID.randomUUID().toString());
+                session.setUsersId(users.getUsersId());
+                session.setToken(UUID.randomUUID().toString());
+                session.setTokenExpiry(Timestamp.from(Instant.now()).toString());
+                session = sessionRepository.save(session);
 
 
-                return new LoginResponse(users.getUsername() + " got login");
-//                return new LoginResponse(users.getUsername() + " got login",  session.getLoginId(), users.getUsersId(), "session.getToken()", session.getTokenExpiry() + "");
+//                return new LoginResponse(users.getUsername() + " got login");
+                return new LoginResponse(users.getUsername() + " got login",  session.getLoginId(), users.getUsersId(), session.getToken(), session.getTokenExpiry() + "");
             }else{
-                return new LoginResponse("Wrong Password");
-//                return new LoginResponse("wrong password",  "session.getLoginId()", "session.getUsersId()", "session.getToken()", "session.getTokenExpiry()");
+//                return new LoginResponse("Wrong Password");
+                return new LoginResponse("wrong password",  "session.getLoginId()", "session.getUsersId()", "session.getToken()", "session.getTokenExpiry()");
             }
         }
 
-        return new LoginResponse("Username does not exist");
-//        return new LoginResponse("not exits",  "session.getLoginId()", "session.getUsersId()", "session.getToken()", "session.getTokenExpiry()");
+//        return new LoginResponse("Username does not exist");
+        return new LoginResponse("not exits",  "session.getLoginId()", "session.getUsersId()", "session.getToken()", "session.getTokenExpiry()");
     }
 
 }

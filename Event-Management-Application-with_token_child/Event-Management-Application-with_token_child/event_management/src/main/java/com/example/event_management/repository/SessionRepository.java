82c707e9e5edfc0541e60563f357d88937d0543c
Gajendra Jangid <89866416+gajendraJangid421@users.noreply.github.com>
2023-06-09 12:37:57 +0530

    Delete Event-Management-Application-with_token_child/Event-Management-Application-with_token_child/event_management directory

--- Event-Management-Application-with_token_child/Event-Management-Application-with_token_child/event_management/src/main/java/com/example/event_management/repository/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.event_management.repository;
-
-import com.example.event_management.model.LoginResponse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<LoginResponse, String> {
-
-}
